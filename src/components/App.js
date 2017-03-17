ca4b00713f574d0b6a21644a6b7dd17f41d70621
Vishal <vishal@conversocial.com>
2017-03-17 22:56:31 +0000

    adding css modules

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import styles from './styles/app.scss'
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>App</div>
+      <div className={styles.container}>
+          <div className={styles.app}>Jai Mata Di</div>
+      </div>
     );
   }
 }
